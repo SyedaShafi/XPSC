de8a1fc1de4558fa79a923932ab1ba979bc3b4f6
SyedaShafi <syedashafi4@gmail.com>
2023-10-21 00:19:30 +0600

    week3 day6

--- week3_practice/a5.cpp
+++ week3_practice/a5.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 #define ll long long int
 using namespace std;
-const int N = 1e7 + 1;
+const int N = 1e9 + 7;
 int main()
 {
-  
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        ll ans = 1;
+        for (int i = 0; i < k; i++)
+        {
+            ans = ((ans * n) % N);
+        }
+        cout << ans << "\n";
+    }
 }
